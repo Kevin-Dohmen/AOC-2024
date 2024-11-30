15e5296e96cb2d3c4ca470c536eb451cf6f5a0e0
Kevin Dohmen <kevindohmen06@gmail.com>
2024-11-30 23:41:09 +0100

    another small tweak to md names

--- genFolders.py
+++ genFolders.py
@@ -4,8 +4,9 @@ def createFolder(i):
     name = f"Day-{i}"
     if not os.path.exists(name):
         os.makedirs(name)
-        with open(name + "/description.md", "w") as f:
-            f.write(f"# [**Advent of Code 2024 - Day {i}**](https://adventofcode.com/2024/day/{i})\n\n## Description\n")
+    
+    with open(name + "/README.md", "w") as f:
+        f.write(f"# [**Advent of Code 2024 - Day {i}**](https://adventofcode.com/2024/day/{i})\n\n## Description\n")
 
 if __name__ == "__main__":
     os.makedirs("Days")
