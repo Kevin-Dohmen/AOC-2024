d90226c33434a485c2606c722788f54313f9569d
Kevin Dohmen <kevindohmen06@gmail.com>
2024-12-02 20:15:59 +0100

    day 2 part 1 done

--- Days/Day-2/part-1.py
+++ Days/Day-2/part-1.py
@@ -1,13 +1,36 @@
-
 def parseInput():
     input = []
 
     with open('input.txt', 'r') as file:
-        input = file.read().splitlines()
-        # some code for case-specific parsing
+        tmp = file.read().splitlines()
+        tmp2 = [i.split(' ') for i in tmp]
+        for item in tmp2:
+            input.append([int(i) for i in item])
         
     return input
 
-
 if __name__ == "__main__":
     input = parseInput()
+    
+    safe = 0
+    for item in input:
+        tmpSafe = True
+        increasing = int(item[1]) >= (item[0])
+        for i in range(len(item) - 1):
+            diff = int(item[i + 1]) - int(item[i])
+            
+            if increasing and diff <= 0:
+                tmpSafe = False
+                break
+
+            if not increasing and diff >= 0:
+                tmpSafe = False
+                break
+
+            if 0 < abs(diff) > 3:
+                tmpSafe = False
+                break
+        if tmpSafe:
+            safe += 1
+    
+    print(safe)
